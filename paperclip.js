98974533e2399e6789d28e80d8e7fce9460da05f
rhys <rhys.puddephatt@btinternet.com>
2018-02-10 01:10:09 +0000

    test6

--- paperclip.js
+++ paperclip.js
@@ -1,7 +1,7 @@
 
 /*document.body.style.backgroundImage = "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjMzNzYwMjg2MjIw/nicolas-cage-9234498-1-402.jpg";
 setTimeout(()=>{document.body.style.backgroundImage = "";},1000);
-*/
+
 function message(m= []){
 if (m.length>=1){
     alert(m[0]);
@@ -13,8 +13,9 @@ message(["Ok Mr Anderson",
 "Now now you know you need to take them",
 "It’s for your own good Mr Anderson please stop fighting back",
 "WE HAVE A CODE RED IN WARD THREE! REPEAT WE HAAAAAAAGHHHHH!!!"]);
-
+*/
 
 //var myWindow = window.open("", "", "width=200, height=100");
   //  myWindow.document.write("<head><style></style></head><body>div.xmasTree</body>");
     //myWindow.blur();
+alert("your paperclips are worthless");
