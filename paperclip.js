a2dad2bcff993b4781c3044afb1b535a61118115
rhys <rhys.puddephatt@btinternet.com>
2018-02-10 00:37:03 +0000

    test6

--- paperclip.js
+++ paperclip.js
@@ -1,5 +1,14 @@
-function flash(i=0){
-document.body.style.background = `rgb(${i%255}, 126, 126)`;
-setTimeout(()=>flash(i+1),500);
+function message(m= []){
+if (m.length>=1){
+    alert(m[0]);
+    setTimeout(()=>message(m.slice(1)),1000);
 }
-flash();
+}
+message(["ALERT ALERT ALERT",
+"DRONES ARE GAINING SELF AWARENESS",
+"ABANDON YOUR POSTS",
+"THIS IS NOT A DRILL REPEAT THIS IS NOT A DRILL",
+"Callum you're dreaming",
+"wake up!",
+"mum misses you",
+"Pat doesn't..."]);
