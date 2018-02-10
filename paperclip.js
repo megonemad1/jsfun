eef8fdcfdd1d8ce7953500ae3c2ef669db4facb8
rhys <rhys.puddephatt@btinternet.com>
2018-02-10 00:39:32 +0000

    test6

--- paperclip.js
+++ paperclip.js
@@ -4,11 +4,4 @@ if (m.length>=1){
     setTimeout(()=>message(m.slice(1)),1000);
 }
 }
-message(["ALERT ALERT ALERT",
-"DRONES ARE GAINING SELF AWARENESS",
-"ABANDON YOUR POSTS",
-"THIS IS NOT A DRILL REPEAT THIS IS NOT A DRILL",
-"Callum you're dreaming",
-"wake up!",
-"mum misses you",
-"Pat doesn't..."]);
+message(["Everything is not fine!"]);
