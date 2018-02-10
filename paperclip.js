74705e2a21f73fa2dd382ee18e19b38594c71009
rhys <rhys.puddephatt@btinternet.com>
2018-02-10 00:50:05 +0000

    test6

--- paperclip.js
+++ paperclip.js
@@ -1,10 +1,11 @@
 
-document.body.style.backgroundImage = "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjMzNzYwMjg2MjIw/nicolas-cage-9234498-1-402.jpg";
+/*document.body.style.backgroundImage = "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTIwNjA4NjMzNzYwMjg2MjIw/nicolas-cage-9234498-1-402.jpg";
 setTimeout(()=>{document.body.style.backgroundImage = "";},1000);
-/*function message(m= []){
+function message(m= []){
 if (m.length>=1){
     alert(m[0]);
     setTimeout(()=>message(m.slice(1)),10000);
 }
 }
 message(["Callum please come back to us", "Visiting time is over now", "we'll come back tomorrow"]);*/
+function flash(){}
